37477a13261597b6e30b07d06828b3ce3cbcb527
mao1106 <wawa8989@gmail.com>
2026-01-24 17:01:36 +0800

    chore: initial project structure and dependencies

---
+++ src/__init__.py
@@ -0,0 +1,17 @@
+"""
+EdgeAI-Gesture-Control Core Package
+====================================
+
+This package contains the core application logic for the EdgeAI Gesture Control system.
+
+Modules:
+    - gesture_control_app: Main application class for gesture recognition and control
+
+Author: EdgeAI Development Team
+Version: 2.1.0 (Sprint 2 - MediaPipe Integration)
+"""
+
+from .gesture_control_app import GestureControlApp
+
+__version__ = "2.1.0"
+__all__ = ["GestureControlApp"]
